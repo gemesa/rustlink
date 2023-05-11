0a22628de76878a033820acf16a5abe588f28e22
Andras Gemes <andrasgemes@outlook.com>
2023-05-11 11:44:10 +0200

    feat: add 'list' subcommand to rst-info

--- src/bin/rst-info.rs
+++ src/bin/rst-info.rs
@@ -1,1 +1,44 @@
-fn main() {}
+use anyhow::{bail, Error, Result};
+use clap::{Parser, Subcommand};
+
+/// Chip and device information tool
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// List ST devices
+    List,
+}
+
+fn list_st_devices() {
+    for device in rusb::devices().unwrap().iter() {
+        let device_desc = device.device_descriptor().unwrap();
+
+        if device_desc.vendor_id() == 0x0483 {
+            println!(
+                "Bus {:03} Device {:03} ID {:04x}:{:04x}",
+                device.bus_number(),
+                device.address(),
+                device_desc.vendor_id(),
+                device_desc.product_id()
+            );
+        }
+    }
+}
+
+fn main() -> Result<(), Error> {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::List) => list_st_devices(),
+        None => bail!("unrecognized command"),
+    }
+
+    Ok(())
+}
