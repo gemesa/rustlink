9cff37fe8c98651e6899cd91de61affc69fc53a2
Andras Gemes <andrasgemes@outlook.com>
2023-05-13 20:18:18 +0200

    feat: make 'list' subcommand more verbose

--- src/bin/rst-info.rs
+++ src/bin/rst-info.rs
@@ -1,6 +1,18 @@
 use anyhow::{bail, Error, Result};
 use clap::{Parser, Subcommand};
 
+use std::time::Duration;
+
+use usb_ids::FromId;
+
+use rusb::{DeviceDescriptor, DeviceHandle, DeviceList, Language, UsbContext};
+
+struct UsbDevice<T: UsbContext> {
+    handle: DeviceHandle<T>,
+    language: Language,
+    timeout: Duration,
+}
+
 const VENDOR_ID_ST: u16 = 0x0483;
 
 /// Chip and device information tool
@@ -18,20 +30,97 @@ enum Commands {
     List,
 }
 
-fn list_st_devices() {
-    for device in rusb::devices().unwrap().iter() {
-        let device_desc = device.device_descriptor().unwrap();
+fn list_st_devices() -> Result<()> {
+    let timeout = Duration::from_secs(1);
+
+    for device in DeviceList::new()?.iter() {
+        let device_desc = match device.device_descriptor() {
+            Ok(d) => d,
+            Err(_) => continue,
+        };
+
+        let mut usb_device = {
+            match device.open() {
+                Ok(h) => match h.read_languages(timeout) {
+                    Ok(l) => {
+                        if !l.is_empty() {
+                            Some(UsbDevice {
+                                handle: h,
+                                language: l[0],
+                                timeout,
+                            })
+                        } else {
+                            None
+                        }
+                    }
+                    Err(_) => None,
+                },
+                Err(_) => None,
+            }
+        };
 
         if device_desc.vendor_id() == VENDOR_ID_ST {
-            println!(
-                "Bus {:03} Device {:03} ID {:04x}:{:04x}",
-                device.bus_number(),
-                device.address(),
-                device_desc.vendor_id(),
-                device_desc.product_id()
-            );
+            print_device(&device_desc, &mut usb_device);
         }
     }
+
+    Ok(())
+}
+
+fn print_device<T: UsbContext>(device_desc: &DeviceDescriptor, handle: &mut Option<UsbDevice<T>>) {
+    let vid: u16 = device_desc.vendor_id();
+    let pid = device_desc.product_id();
+
+    let vendor_name = match usb_ids::Vendor::from_id(device_desc.vendor_id()) {
+        Some(vendor) => vendor.name(),
+        None => "Unknown vendor",
+    };
+
+    let product_name =
+        match usb_ids::Device::from_vid_pid(device_desc.vendor_id(), device_desc.product_id()) {
+            Some(product) => product.name(),
+            None => "Unknown product",
+        };
+
+    println!("Device Descriptor:");
+    println!(
+        "  bcdUSB             {:2}.{}{}",
+        device_desc.usb_version().major(),
+        device_desc.usb_version().minor(),
+        device_desc.usb_version().sub_minor()
+    );
+    println!("  idVendor          {vid:#06x} {vendor_name}",);
+    println!("  idProduct         {pid:#06x} {product_name}",);
+    println!(
+        "  bcdDevice          {:2}.{}{}",
+        device_desc.device_version().major(),
+        device_desc.device_version().minor(),
+        device_desc.device_version().sub_minor()
+    );
+    println!(
+        "  iManufacturer        {:3} {}",
+        device_desc.manufacturer_string_index().unwrap_or(0),
+        handle.as_mut().map_or(String::new(), |h| h
+            .handle
+            .read_manufacturer_string(h.language, device_desc, h.timeout)
+            .unwrap_or_default())
+    );
+    println!(
+        "  iProduct             {:3} {}",
+        device_desc.product_string_index().unwrap_or(0),
+        handle.as_mut().map_or(String::new(), |h| h
+            .handle
+            .read_product_string(h.language, device_desc, h.timeout)
+            .unwrap_or_default())
+    );
+    println!(
+        "  iSerialNumber        {:3} {}",
+        device_desc.serial_number_string_index().unwrap_or(0),
+        handle.as_mut().map_or(String::new(), |h| h
+            .handle
+            .read_serial_number_string(h.language, device_desc, h.timeout)
+            .unwrap_or_default())
+    );
 }
 
 fn main() -> Result<(), Error> {
@@ -41,6 +130,4 @@ fn main() -> Result<(), Error> {
         Some(Commands::List) => list_st_devices(),
         None => bail!("unrecognized command"),
     }
-
-    Ok(())
 }
